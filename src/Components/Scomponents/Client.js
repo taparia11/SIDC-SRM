f370ba6583ded85f111fc033f21bf2d6cf633830
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-19 01:51:26 +0530

    Component Created and working Fine

---
+++ src/Components/Scomponents/Client.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const Client = () => {
+  return (
+    <>
+    <section id="clients" className="clients">
+      <div className="container" data-aos="fade-up">
+
+        <div className="section-title">
+          <h2>Clients</h2>
+        </div>
+
+        <div className="row no-gutters clients-wrap clearfix" data-aos="fade-up">
+
+          <div className="col-lg-3 col-md-4 col-6">
+            <div className="client-logo">
+              <img src="assets/img/clients/client-1.png" className="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-4 col-6">
+            <div className="client-logo">
+              <img src="assets/img/clients/client-2.png" className="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-4 col-6">
+            <div className="client-logo">
+              <img src="assets/img/clients/client-3.png" className="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-4 col-6">
+            <div className="client-logo">
+              <img src="assets/img/clients/client-4.png" className="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-4 col-6">
+            <div className="client-logo">
+              <img src="assets/img/clients/client-5.png" className="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-4 col-6">
+            <div className="client-logo">
+              <img src="assets/img/clients/client-6.png" className="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-4 col-6">
+            <div className="client-logo">
+              <img src="assets/img/clients/client-7.png" className="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-4 col-6">
+            <div className="client-logo">
+              <img src="assets/img/clients/client-8.png" className="img-fluid" alt="">
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default Client
