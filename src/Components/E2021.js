f102229b9cc0c27cf952155b6ec43c6aad574f40
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-19 01:51:38 +0530

    Component Created and working Fine

---
+++ src/Components/E2021.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react'
+
+const Ps = (props) => {
+    useEffect(() => {
+        document.title = `${props.titleChange} - SIDC`;
+        //eslint-disable-next-line
+    }, [])
+  return (
+    <>
+    <section id="breadcrumbs" className="breadcrumbs">
+      <div className="container">
+
+        <div className="d-flex justify-content-between align-items-center">
+          <h2>EVENTS 2021</h2>
+          <ol>
+            <li><a href="/">Home</a></li>
+            <li>EVENTS 2021</li>
+          </ol>
+        </div>
+
+      </div>
+    </section>
+    <div className='container' style={{textAlign:'center'}}> <br /><br /><br /><br /><br /><br /><br />
+        <h1>Welcome to EVENTS 2021</h1> <br /><br /><br /><br /><br /><br />
+    </div>
+    </>
+  )
+}
+
+export default Ps
