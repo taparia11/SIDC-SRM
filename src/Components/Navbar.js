462c59e4b904e425fff6ac70c7ac0f0bd12793bb
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-05-11 17:35:50 +0530

    basic updates

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header id="header" className="fixed-top">
     <div className="container d-flex align-items-center">
 
-      <h1 className="logo me-auto"><a href="/"><span>SI</span>DC</a></h1>
+      <h1 className="logo me-auto"><a href="/"><span>CI</span>AP</a></h1>
       {/* <a href="index.html" className="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" className="img-fluid"></a> */}
 
       <nav id="navbar" className="navbar order-last order-lg-0">
