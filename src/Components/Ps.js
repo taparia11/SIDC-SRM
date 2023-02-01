c1c1e44fbd33cb55467ee1effea7dbbaf1961e5b
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-02-01 20:31:29 +0530

    patent stat updated

--- src/Components/Ps.js
+++ src/Components/Ps.js
@@ -20,8 +20,53 @@ const Ps = (props) => {
 
       </div>
     </section>
-    <div className='container' style={{textAlign:'center'}}> <br /><br /><br /><br /><br /><br /><br />
-        <h1>Welcome to PATENT STATISTICS</h1> <br /><br /><br /><br /><br /><br />
+    <div className='container' style={{textAlign:'center'}}> 
+    <br />  <br />  <br />  <br />  <br />
+  <div class="table-responsive">
+    <table class="table table-borderless table-hover">
+      <thead>
+        <tr className='table-danger'>
+          <th scope="col">Country</th>
+          <th scope="col">Country	Count of Country</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="">
+          <td scope="row"> Australia</td>
+          <td>13</td>
+        </tr>
+        <tr className='table-active'>
+          <td  scope="row"><b>India</b></td>
+          <td><b>636</b></td>
+        </tr>
+        <tr class="">
+          <td scope="row">JAPAN</td>
+          <td>1</td>
+        </tr>
+        <tr class="">
+          <td scope="row">Korea</td>
+          <td>2</td>
+        </tr>
+        <tr class="">
+          <td scope="row">PCT</td>
+          <td>1</td>
+        </tr>
+        <tr class="">
+          <td scope="row">Singapore</td>
+          <td>1</td>
+        </tr>
+        <tr class="">
+          <td scope="row">US</td>
+          <td>2</td>
+        </tr>
+        <tr class="table-danger">
+          <td scope="row">Grand Total</td>
+          <td>656</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <br />  <br />  <br />  <br />
     </div>
     </>
   )
