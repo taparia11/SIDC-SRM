ef2e05c5428af1a0802e08a7131a0360f357a844
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2023-02-01 20:31:20 +0530

    Year wise patent updated

--- src/Components/Ywps.js
+++ src/Components/Ywps.js
@@ -20,8 +20,57 @@ const Ps = (props) => {
 
       </div>
     </section>
-    <div className='container' style={{textAlign:'center'}}> <br /><br /><br /><br /><br /><br /><br />
-        <h1>Welcome to YEAR WISE PATENT STATISTICS</h1> <br /><br /><br /><br /><br /><br />
+    <div className='container' style={{textAlign:'center'}}> <br /><br />
+    <table class="table table-bordered border-primary">
+  <thead>
+    <tr class="table-primary">
+      <th scope="col">Year</th>
+      <th scope="col">NO.OF PATENTS</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>2012</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>2015</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>2016</td>
+      <td>6</td>
+    </tr>
+    <tr>
+      <td>2017</td>
+      <td>79</td>
+    </tr>
+    <tr>
+      <td>2018</td>
+      <td>120</td>
+    </tr>
+    <tr>
+      <td>2019</td>
+      <td>62</td>
+    </tr>
+    <tr>
+      <td>2020</td>
+      <td>210</td>
+    </tr>
+    <tr>
+      <td>2021</td>
+      <td>61</td>
+    </tr>
+    <tr>
+      <td>2022</td>
+      <td>116</td>
+    </tr>
+    <tr class="table-primary">
+      <td>Grand Total</td>
+      <td>656</td>
+    </tr>
+  </tbody>
+</table>
     </div>
     </>
   )
